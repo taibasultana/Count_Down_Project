e56fc5faa0fdffa2e5256465b09a61bc515056b1
taibasultana <tsmtaibasultana@gmail.com>
2023-03-10 18:47:03 +0600

    First

---
+++ test.py
@@ -0,0 +1,15 @@
+txt = "hello, my name, is Peter, I am 26 years old"
+
+x = txt.split(", ")
+
+print(x)
+
+
+user_input=input("enter your goal with a deadline separated by colon\n")
+
+input_list= user_input.split(":")
+print(input_list)
+
+print(input_list[0])
+
+print(input_list[1])
